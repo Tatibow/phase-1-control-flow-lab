418da1d75f522d92ad1322acceef1929e6a68e1d
Tatiana <Bowmantatiana2@gmail.com>
2022-12-12 14:12:16 -0800

    commit tree a83f0a8b5f6e608c2f79e505b090fe1f39e248cd

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someVal) {
+  if (someVal <= 400) {
+    return 'This one is on me!';
+  } else if (someVal >= 400 && someVal <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (someVal > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if(someVal > 2500) {
+    return "No can do.";
+  }
+}
+scuberGreetingForFeet(199)
+scuberGreetingForFeet(1500)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501)
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ternaryCheckCity('Pittsburgh')
+ternaryCheckCity("NYC")
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+    return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+     return "Bye.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip("generous")
+switchOnCharmFromTip("not as generous")
+switchOnCharmFromTip("thanks for eveything")
